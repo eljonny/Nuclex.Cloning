b8f1d52dcc92d538890e62c54d6c5af8a6c59f87
Jonathan Hyry <jonathan.hyry@outlook.com>
2025-05-06 23:30:06 -0700

    Project upgrade to .Net Standard 2.0 SDK style, format nuspec, asmInfo
    AssemblyInfo information moved to the project file during the upgrade.
    
    Formatted nuspec file.
    
    Added a basic project icon that looks like an object being cloned.
    Add the readme to the NuGet package.
    Update the target framework to netstandard2.0.
    Moved all the nuget package information to the project file.
    Updated copyright.
    Version bump to 1.0.2.
    Added release notes.
    Changed the license to a license expression for the CPL version 1,
     based on the SPDX license expression for that license.
    Updated the project URL and repository URL.
    Add configs to always build symbol packages, and to use the snupkg
     format.

--- Nuclex.Cloning/Properties/AssemblyInfo.cs
+++ Nuclex.Cloning/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Nuclex.Cloning")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nuclex.Cloning")]
-[assembly: AssemblyCopyright("Copyright (C) 2002-2012 Nuclex Development Labs")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c13871f0-f2ad-4207-a55f-3cc2fe8db753")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
